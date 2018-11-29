756978ab3edd95baf8309a733483f2ad6a7f1d70
CJaiK <43978450+CJaiK@users.noreply.github.com>
2018-11-29 14:58:46 -0800

    Add files via upload

--- Labeling.py
+++ Labeling.py
@@ -7,7 +7,6 @@ letters = np.matrix(('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'
 with open('C:/Users/cjaik/Desktop/ASL/labeled_asl.csv',mode='w') as file:
     file_writer = csv.writer(file,delimiter=',')
     for i in range(len(letters)):
-        #currentLabel = np.empty([3000,40001])
         for j in range(0,3000):
             img = Image.open('C:/Users/cjaik/Desktop/ASL/grayed_asl/grayed_asl_train/'+str(letters[0,i])+'+/'+str(letters[0,i])+str(j+1)+'+.jpg')
             imgArray = np.array(img)
