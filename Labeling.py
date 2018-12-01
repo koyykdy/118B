ff60a046745c62238574d8ecd4a0af0552b4739d
CJaiK <43978450+CJaiK@users.noreply.github.com>
2018-12-01 14:50:54 -0800

    Add files via upload

--- Labeling.py
+++ Labeling.py
@@ -2,13 +2,13 @@ from PIL import Image
 import numpy as np
 import csv
 
-letters = np.matrix(('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','nothing'))
+letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','nothing']
 
-with open('C:/Users/cjaik/Desktop/ASL/labeled_asl.csv',mode='w') as file:
+with open('C:/Users/cjaik/Desktop/ASL/labeled_asl.csv', newline = '', mode='w') as file:
     file_writer = csv.writer(file,delimiter=',')
-    for i in range(len(letters)):
+        print(letters[i])
         for j in range(0,3000):
-            img = Image.open('C:/Users/cjaik/Desktop/ASL/grayed_asl/grayed_asl_train/'+str(letters[0,i])+'+/'+str(letters[0,i])+str(j+1)+'+.jpg')
+            img = Image.open('C:/Users/cjaik/Desktop/ASL/grayed_asl/grayed_asl_train/'+str(letters[i])+'+/'+str(letters[i])+str(j+1)+'+.jpg')
             imgArray = np.array(img)
             flattened = np.ravel(imgArray)
             incLabel = np.append(i,flattened)
